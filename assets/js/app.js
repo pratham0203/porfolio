79a2f0a44b4f761f07e51b2dbe98b29051301fb8
Pratham Tripathi <prathamtripathi011@gmail.com>
2021-07-27 10:03:15 +0530

    assets

---
+++ assets/js/app.js
@@ -0,0 +1,15 @@
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyCQuOfcA_LQV3wM7umlrGTEdjExHp0jMz4",
+    authDomain: "prathamtripathi11-c82e2.firebaseapp.com",
+    projectId: "prathamtripathi11-c82e2",
+    storageBucket: "prathamtripathi11-c82e2.appspot.com",
+    messagingSenderId: "35422747769",
+    appId: "1:35422747769:web:2ff2e8b8064641934ca8a8",
+    measurementId: "G-26HER1EGGP"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
